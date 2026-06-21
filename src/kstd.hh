8c9cb37faab6f3913df00c850db32670b7e67a26
Maciej Urban <maciej-urban@outlook.com>
2026-06-21 18:29:58 +0200

    Bounded_Array

--- src/kstd.hh
+++ src/kstd.hh
@@ -2,7 +2,9 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <new>
 #include <source_location>
+#include <utility>
 
 #include "int.hh"
 
@@ -68,6 +70,83 @@ inline void debug_assert(
         asm volatile("hlt");
     }
 }
+
+//
+// Use as a normal Array. The exception is this can't grow, because it's backed by static memory.
+//
+template <typename T, usize N>
+struct Bounded_Array {
+    static constexpr auto MAX_SIZE = N;
+    usize size = 0;
+    T data[N];
+
+    auto operator[](u64 index, const std::source_location& location = std::source_location::current()) -> T& {
+        assert(index < size, "index out of bounds", location);
+        return data[index];
+    }
+
+    auto push_back(T&& element) -> void {
+        assert(size < MAX_SIZE);
+        ::new (data + size) T(std::move(element));
+        ++size;
+    }
+
+    auto push_back(const T& element) -> void {
+        assert(size < MAX_SIZE);
+        ::new (data + size) T(element);
+        ++size;
+    }
+
+    struct iterator {
+        T* ptr;
+        iterator(T* p)
+            : ptr(p) {}
+        T& operator*() {
+            return *ptr;
+        }
+        iterator& operator++() {
+            ++ptr;
+            return *this;
+        }
+        bool operator!=(const iterator& other) const {
+            return ptr != other.ptr;
+        }
+    };
+
+    struct const_iterator {
+        const T* ptr;
+        explicit const_iterator(const T* p)
+            : ptr(p) {}
+        const T& operator*() const {
+            return *ptr;
+        }
+        const_iterator& operator++() {
+            ++ptr;
+            return *this;
+        }
+        bool operator!=(const const_iterator& other) const {
+            return ptr != other.ptr;
+        }
+
+        // Implicit conversion from iterator to const_iterator
+        explicit const_iterator(iterator it)
+            : ptr(it.ptr) {}
+    };
+
+    iterator begin() {
+        return iterator(data);
+    }
+    iterator end() {
+        return iterator(data + size);
+    }
+
+    const_iterator cbegin() const noexcept {
+        return const_iterator(data);
+    }
+    const_iterator cend() const noexcept {
+        return const_iterator(data + size);
+    }
+};
 
 template <typename T, usize N>
 struct Static_Array {
