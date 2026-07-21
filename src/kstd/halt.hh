71861297c2eed68f029d88f4c9bdb4a6adfe6f7e
Maciej Urban <maciejj.urban@gmail.com>
2026-07-21 11:38:08 +0200

    decouple halt from term

--- src/kstd/halt.hh
+++ src/kstd/halt.hh
@@ -4,28 +4,14 @@
 
 #include "serial.hh"
 
+using Halt_Fn = auto (*)(const char* file, u32 line, u32 col, const char* message) -> void;
+inline Halt_Fn __halt_print;
+
+force_inline auto halt_set_print(Halt_Fn fn) -> void {
+    __halt_print = fn;
+}
+
 static bool __panicking = false;
-static bool __term_ready = false;
-
-// Must only be called after term::initialize() succeeds.
-force_inline auto
-halt_set_term_ready() -> void {
-    __term_ready = true;
-}
-
-// Forward declarations.
-namespace term {
-    auto print(const char* format) -> int;
-
-    template <typename T, typename... Rest>
-    auto print(const char* format, T&& value, Rest&&... rest) -> int;
-
-    auto println() -> int;
-    auto println(const char* format) -> int;
-
-    template <typename T, typename... Rest>
-    auto println(const char* format, T&& value, Rest&&... rest) -> int;
-}
 
 [[noreturn]] static auto
 halt_forever(const char* message, const std::source_location& location = std::source_location::current()) -> void {
@@ -38,10 +24,8 @@ halt_forever(const char* message, const std::source_location& location = std::so
     serial::print("%:%:%", location.file_name(), location.line(), location.column());
     if (message != nullptr) serial::println(": %", message);
 
-    // Only safe once term has actually confirmed init succeeded.
-    if (__term_ready) {
-        term::print("%:%:%", location.file_name(), location.line(), location.column());
-        if (message != nullptr) term::println(": %", message);
+    if (__halt_print) {
+        __halt_print(location.file_name(), location.line(), location.column(), message);
     }
 
     for (;;) asm volatile("hlt");
