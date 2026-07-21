9c089b7c08f56ad9f81648c50ba490ee315309a0
Maciej Urban <maciejj.urban@gmail.com>
2026-07-21 10:54:43 +0200

    flatten term forward declarations

--- src/kstd/halt.hh
+++ src/kstd/halt.hh
@@ -13,23 +13,21 @@ halt_set_term_ready() -> void {
     __term_ready = true;
 }
 
-#define TERM_FORWARD_DECLARATIONS()                                         \
-    namespace term {                                                        \
-        auto print(const char* format) -> int;                              \
-                                                                            \
-        template <typename T, typename... Rest>                             \
-        auto print(const char* format, T&& value, Rest&&... rest) -> int;   \
-                                                                            \
-        auto println() -> int;                                              \
-        auto println(const char* format) -> int;                            \
-                                                                            \
-        template <typename T, typename... Rest>                             \
-        auto println(const char* format, T&& value, Rest&&... rest) -> int; \
-    }
+// Forward declarations.
+namespace term {
+    auto print(const char* format) -> int;
 
-TERM_FORWARD_DECLARATIONS()
+    template <typename T, typename... Rest>
+    auto print(const char* format, T&& value, Rest&&... rest) -> int;
 
-[[noreturn]] static auto 
+    auto println() -> int;
+    auto println(const char* format) -> int;
+
+    template <typename T, typename... Rest>
+    auto println(const char* format, T&& value, Rest&&... rest) -> int;
+}
+
+[[noreturn]] static auto
 halt_forever(const char* message, const std::source_location& location = std::source_location::current()) -> void {
     if (__panicking) {
         for (;;) asm volatile("hlt");
