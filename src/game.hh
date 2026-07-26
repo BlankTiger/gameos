3a1fc793997d1b2498b6c3ad6c1f4c376bfc4056
Maciej Urban <maciejj.urban@gmail.com>
2026-07-26 15:30:30 +0200

    grid

--- src/game.hh
+++ src/game.hh
@@ -8,69 +8,20 @@
 #include "kstd/ps2.hh"
 #include "kstd/string.hh"
 
-constexpr f32 PLAYER_SPEED_PX_PER_SEC = 300.0f;
-constexpr u64 FPS_MAX                 = 144;
-
-struct Player {
-    f32 x;
-    f32 y;
-    Resource_View sprite;
-};
+constexpr u64 FPS_MAX = 144;
 
 struct Game {
-    Player player;
-    u64    time_ms    = 0;
-    f64    dt         = 0.0;
-    u64    dt_real    = 0;
-    f64    time_scale = 1.0;
-    f64    fps        = 0.0;
+    u64 time_ms    = 0;
+    f64 dt         = 0.0;
+    u64 dt_real    = 0;
+    f64 time_scale = 1.0;
+    f64 fps        = 0.0;
 };
 
-auto update_player(Player& player, f64 dt) -> void {
-    f32 dx = 0.0f;
-    f32 dy = 0.0f;
-    if (ps2::is_pressed(ps2::Scancode::W)) dy -= 1.0f;
-    if (ps2::is_pressed(ps2::Scancode::S)) dy += 1.0f;
-    if (ps2::is_pressed(ps2::Scancode::A)) dx -= 1.0f;
-    if (ps2::is_pressed(ps2::Scancode::D)) dx += 1.0f;
-
-    const auto max_x = static_cast<f32>(gfx::width()  - player.sprite.width);
-    const auto max_y = static_cast<f32>(gfx::height() - player.sprite.height);
-
-    const auto scale = PLAYER_SPEED_PX_PER_SEC * dt;
-    player.x = std::clamp(static_cast<f32>(player.x + dx * scale), f32{0}, max_x);
-    player.y = std::clamp(static_cast<f32>(player.y + dy * scale), f32{0}, max_y);
-}
-
-auto draw_player(const Player& player) -> void {
-    gfx::draw_sprite(player.sprite, player.x, player.y);
-}
-
-auto update(Game& game) -> void {
-    update_player(game.player, game.dt);
-}
+auto update(Game& game) -> void {}
 
 auto draw(const Game& game) -> void {
     gfx::clear(gfx::BLACK);
-
-    {
-        gfx::draw_rect(250, 250, 100, 100, gfx::BLUE);
-        gfx::draw_rect(300, 300, 80,  80,  gfx::Color{0, 0, 200, 128});
-
-        gfx::draw_rect(400, 250, 100, 100, gfx::GREEN);
-        gfx::draw_rect(450, 300, 80,  80,  gfx::Color{0, 200, 0, 128});
-
-        gfx::draw_rect(550, 250, 100, 100, gfx::RED);
-        gfx::draw_rect(600, 300, 80,  80,  gfx::Color{200, 0, 0, 128});
-
-        gfx::draw_rect(700, 250, 100, 100, gfx::WHITE);
-        gfx::draw_rect(750, 300, 80,  80,  gfx::Color{230, 230, 230, 128});
-
-        gfx::draw_rect(250, 400, 550, 300, gfx::WHITE);
-        gfx::draw_circle(525, 550, 100, gfx::RED);
-    }
-
-    draw_player(game.player);
 
     const auto fps_text = sprint("FPS: %", game.fps);
     gfx::draw_text(8, 8, fps_text);
@@ -79,12 +30,7 @@ auto draw(const Game& game) -> void {
 }
 
 auto game_main() -> void {
-    Player player{
-        .x      = static_cast<f32>(gfx::width())  / 2,
-        .y      = static_cast<f32>(gfx::height()) / 2,
-        .sprite = @embed("cool.png"),
-    };
-    Game game{player};
+    Game game;
 
     u64 last_tick = time::get_ticks();
 
--- src/kstd/math.hh
+++ src/kstd/math.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "basic.hh"
+#include "array.hh"
+#include "assert.hh"
 
 namespace math {
 
@@ -36,5 +38,125 @@ struct Rect {
         y2 = (y2 >= screen_height) ? screen_height - 1 : y2;
     }
 };
+
+// rows (top to bottom)
+// cols (left to right)
+// layers (top to bottom)
+template <typename T = bool>
+struct Grid3 {
+    Array<T> backing_array;
+    T default_value;
+    u32 rows, cols, layers;
+    u32 cells_in_layer;
+
+    Grid3(u32 rows, u32 cols, u32 layers, T&& default_value = {})
+        : backing_array(Array<T>{rows * cols * layers}),
+          default_value(default_value),
+          rows(rows),
+          cols(cols),
+          layers(layers),
+          cells_in_layer(rows * cols) {
+        for (u32 index = 0; index < rows * cols * layers; ++index) {
+            backing_array.push_back(default_value);
+        }
+    }
+
+    auto set(u32 row, u32 col, u32 layer, T&& value) -> void {
+        const auto index = backing_array_index_for(row, col, layer);
+        backing_array[index] = value;
+    }
+
+    auto clear(u32 row, u32 col, u32 layer) -> void {
+        const auto index = backing_array_index_for(row, col, layer);
+        backing_array[index] = default_value;
+    }
+
+    auto move(u32 from_row, u32 from_col, u32 from_layer, u32 to_row, u32 to_col, u32 to_layer) -> void {
+        const auto from_index = backing_array_index_for(from_row, from_col, from_layer);
+        const auto to_index   = backing_array_index_for(to_row,   to_col,   to_layer);
+        backing_array[to_index]   = std::move(backing_array[from_index]);
+        backing_array[from_index] = default_value;
+    }
+
+    auto layer_filled_with(u32 layer, T&& value) const -> bool {
+        for (u32 index = layer * cells_in_layer; index < (layer + 1) * cells_in_layer; ++index) {
+            if (backing_array[index] != value) return false;
+        }
+        return true;
+    }
+
+    force_inline auto at(u32 row, u32 col, u32 layer) const -> T {
+        const auto index = backing_array_index_for(row, col, layer);
+        return backing_array[index];
+    }
+
+    force_inline auto backing_array_index_for(u32 row, u32 col, u32 layer) const -> usize {
+        return cells_in_layer * layer + row * cols + col;
+    }
+};
+
+#ifdef UNIT_TESTS
+
+TEST(Grid3_bool, can_set_a_custom_default_value) {
+    {
+        Grid3 grid(3, 3, 3);
+        EXPECT_EQ(grid.at(0, 0, 0), bool{});
+        EXPECT_EQ(grid.at(0, 0, 0), false);
+    }
+
+    {
+        Grid3 grid(3, 3, 3, false);
+        EXPECT_EQ(grid.at(0, 0, 0), bool{});
+        EXPECT_EQ(grid.at(0, 0, 0), false);
+    }
+
+    {
+        Grid3 grid(3, 3, 3, true);
+        EXPECT_EQ(grid.at(0, 0, 0), true);
+    }
+}
+
+TEST(Grid3_bool, can_mark_points_as_occupied) {
+    Grid3 grid(3, 3, 3);
+    EXPECT_EQ(grid.backing_array.size, 27);
+
+    grid.set(1, 1, 1, true);
+    EXPECT_TRUE(grid.at(1, 1, 1));
+}
+
+TEST(Grid3_bool, can_free_points) {
+    Grid3 grid(3, 3, 3);
+    grid.set(1, 1, 1, true);
+    EXPECT_TRUE(grid.at(1, 1, 1));
+
+    grid.clear(1, 1, 1);
+    EXPECT_FALSE(grid.at(1, 1, 1));
+}
+
+TEST(Grid3_bool, can_move_points) {
+    Grid3 grid(3, 3, 3);
+    grid.set(1, 1, 1, true);
+    EXPECT_TRUE(grid.at(1, 1, 1));
+
+    grid.move(1, 1, 1, 2, 2, 2);
+    EXPECT_FALSE(grid.at(1, 1, 1));
+    EXPECT_TRUE(grid.at(2, 2, 2));
+}
+
+TEST(Grid3_bool, can_check_for_a_layer_filled_with_value) {
+    Grid3 grid(2, 2, 2);
+    EXPECT_TRUE(grid.layer_filled_with(0, false));
+
+    grid.set(0, 0, 0, true);
+    grid.set(0, 1, 0, true);
+    EXPECT_FALSE(grid.layer_filled_with(0, true));
+    EXPECT_FALSE(grid.layer_filled_with(0, false));
+
+    grid.set(1, 0, 0, true);
+    grid.set(1, 1, 0, true);
+    EXPECT_TRUE(grid.layer_filled_with(0, true));
+}
+
+#endif
 
 }  // namespace math
