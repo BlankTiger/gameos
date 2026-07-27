235b82dc641a3e57ab6a114f8a7b6a8e9d911577
Maciej Urban <maciejj.urban@gmail.com>
2026-07-27 14:05:41 +0200

    structural refactor of Falling_Body, keep it sorted so it falls correctly

--- src/game.hh
+++ src/game.hh
@@ -11,12 +11,29 @@
 #include "kstd/ps2.hh"
 #include "kstd/math.hh"
 #include "kstd/string.hh"
+#include "kstd/random.hh"
 
 constexpr u64 FPS_MAX                                 = 144;
 constexpr u64 ARBITRARY_FALLING_BODY_BLOCK_SIZE_LIMIT = 10;
-using Block_Coords = std::tuple<u32, u32, u32>;
-using Body         = std::initializer_list<Block_Coords>;
-using Falling_Body = Bounded_Array<Block_Coords, ARBITRARY_FALLING_BODY_BLOCK_SIZE_LIMIT>;
+
+struct Block_Coords {
+    u32 row, col, layer;
+
+    auto operator==(const Block_Coords& other) -> bool {
+        return row == other.row && col == other.col && layer == other.layer;
+    }
+};
+using Body = std::initializer_list<Block_Coords>;
+
+struct Falling_Body {
+    Bounded_Array<Block_Coords, ARBITRARY_FALLING_BODY_BLOCK_SIZE_LIMIT> blocks;
+
+    auto layer_sort() -> void {
+        std::sort(blocks.begin(), blocks.end(), [](const Block_Coords& a, const Block_Coords& b) {
+            return a.layer > b.layer;
+        });
+    }
+};
 
 constexpr std::initializer_list<Body> available_bodies = {
     Body{{0, 0, 0}, {0, 0, 1}},
@@ -44,24 +61,54 @@ struct Game {
 auto set_new_falling_body(Game& game, Falling_Body body) -> bool {
     game.falling_body = body;
 
-    for (const auto& [row, col, layer] : body) {
+    for (const auto& [row, col, layer] : body.blocks) {
         if (game.grid.at(row, col, layer)) return false;
     }
 
-    for (const auto& [row, col, layer] : body) {
+    for (const auto& [row, col, layer] : body.blocks) {
         game.grid.set(row, col, layer, true);
     }
     return true;
 }
 
-auto create_new_falling_body(std::initializer_list<std::tuple<u32, u32, u32>> blocks) -> Falling_Body {
+auto create_new_falling_body(Body blocks) -> Falling_Body {
     Falling_Body body;
 
     for (const auto& block : blocks) {
-        body.push_back(block);
+        body.blocks.push_back(block);
     }
+    body.layer_sort();
 
     return body;
+}
+
+auto get_new_falling_body() -> Falling_Body {
+    const auto max          = available_bodies.size() - 1;
+    const auto body_index   = rand::generate(0, max);
+    const auto body         = available_bodies.begin()[body_index];
+    const auto falling_body = create_new_falling_body(body);
+
+    return falling_body;
+}
+
+auto falling_body_can_go_lower(const Game& game) -> bool {
+    for (const auto& [row, col, layer] : game.falling_body.blocks) {
+        if (game.grid.can_move_lower(row, col, layer)) continue;
+
+        const auto next_layer = layer + 1;
+        bool block_exists_in_this_body = false;
+        // @TODO: optimize maybe? Don't think it's necessary.
+        for (const auto& block_coords : game.falling_body.blocks) {
+            if (block_coords == Block_Coords{row, col, next_layer}) {
+                block_exists_in_this_body = true;
+                break;
+            }
+        }
+
+        if (!block_exists_in_this_body) return false;
+    }
+
+    return true;
 }
 
 auto update(Game& game) -> void {
@@ -76,16 +123,12 @@ auto update(Game& game) -> void {
     // If timer elapsed or a button has been pushed then move down, or if it's
     // not possible solidify into the stationary layers at the bottom.
     if (timer_elapsed || fast_forward_key_pressed) {
-        bool falling_body_can_move_lower = true;
-        for (const auto& [row, col, layer] : game.falling_body) {
-            if (!game.grid.can_move_lower(row, col, layer)) {
-                falling_body_can_move_lower = false;
-                break;
-            }
-        }
+        if (falling_body_can_go_lower(game)) {
+            // This has to go in layer sorted order (bottom-most to top-most layer).
+            // falling body should have already been sorted, but it doesn't cost us much to make sure:
+            game.falling_body.layer_sort();
 
-        if (falling_body_can_move_lower) {
-            for (auto& [row, col, layer] : game.falling_body) {
+            for (auto& [row, col, layer] : game.falling_body.blocks) {
                 game.grid.clear(row, col, layer);
                 layer += 1;
                 game.grid.set(row, col, layer, true);
@@ -94,13 +137,13 @@ auto update(Game& game) -> void {
         else {
             // Solidify into bottom-most layers.
             // Get and set the next falling_body.
+
+            // If there is a new full layer, then clear it and move everything above it
+            // lower by a layer.
         }
 
         game.current_move_started_at_tick = get_ticks();
     }
-
-    // If there is a new full layer, then clear it and move everything above it
-    // lower by a layer.
 }
 
 auto draw(const Game& game) -> void {
@@ -118,7 +161,7 @@ auto draw(const Game& game) -> void {
 auto game_main() -> void {
     Game game;
 
-    Falling_Body body = create_new_falling_body({{0, 0, 0}});
+    Falling_Body body = get_new_falling_body();
     set_new_falling_body(game, body);
 
     u64 last_tick = time::get_ticks();
