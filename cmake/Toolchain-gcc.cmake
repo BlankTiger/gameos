5b423c08da0d4f55bfb1ca956f676ed159e5a625
Maciej Urban <maciejj.urban@gmail.com>
2026-07-30 22:57:22 +0200

    mold

--- cmake/Toolchain-gcc.cmake
+++ cmake/Toolchain-gcc.cmake
@@ -2,3 +2,9 @@ set(CMAKE_SYSTEM_NAME Linux)
 
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
+
+if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    set(CMAKE_EXE_LINKER_FLAGS_INIT    "-fuse-ld=mold")
+    set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")
+    set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=mold")
+endif()
