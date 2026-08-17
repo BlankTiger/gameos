1a5bb014c7d6c70bf2e3627cc7982647bddc4898
Maciej Urban <maciejj.urban@gmail.com>
2026-08-17 17:02:29 +0200

    correct main

--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,8 @@
 
 extern "C" auto kernel_main(u32 magic, const boot::Multiboot2_Info* mbi) -> void {
     kernel_startup(magic, mbi);
-    stack_trace_main();
+    // stack_trace_main();
     // cubes_main();
-    // tetris_main();
+    tetris_main();
     // blocks_tests_main();
 }
